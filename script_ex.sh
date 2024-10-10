143616aad5470319c5e88574f7fb07270bf0f9a3
jhensl19 <jhensl19@charlotte.edu>
2024-10-10 01:48:39 -0400

    Add files via upload

---
+++ script_ex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+whoami
+pwd
+echo "Hello world! This is my first bash script!" 
+echo -n "I am executing the script with user:"
+whoami
+echo -n "I am currently running in the directory:"
+pwd
+exit 0
